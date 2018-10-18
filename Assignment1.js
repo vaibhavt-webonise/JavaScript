6d7515e594cb2313e0d54817257a8b060a72ff88
Vaibhav Tapadiya <vaibhav.tapadiya@weboniselab.com>
2018-10-18 17:37:54 +0530

    Correct Indentation

--- Assignment1.js
+++ Assignment1.js
@@ -1,18 +1,16 @@
 var ar=[5,4,2,8,2,1,4,5,8,5,2,8,8,8];
+
 var count=0;
-for(i=0;i<=9;i++)
-{
-	for(j=0;j<ar.length;j++)
-	{
-		if(ar[j]==i)
-		{
-			count++;
-		}
-	}
-	if(count>0)
-	{
-		console.log(i+" "+count);
-	}
-	count=0;
+
+  for(i=0;i<=9;i++) {
+    for(j=0;j<ar.length;j++) {
+      if(ar[j]==i) {
+        count++;
+      }
+  }
+  if(count>0) {
+    console.log(i+" "+count);
+  }
+  count=0;
 }
 
--- Assignment3/Tabs.js
+++ Assignment3/Tabs.js
@@ -1,35 +1,41 @@
 var t1,t2,t3,t4,t5;
+
 function showTabs() {
-	var w1=window.open("http://www.google.com");
+
+  var w1=window.open("http://www.google.com");
+
   var w2=window.open("http://www.flipkart.com");
-	var w3=window.open("http://www.weboniselab.com");
-	var w4=window.open("https://www.amazon.com/");
-	var w5=window.open("https://www.snapdeal.com/");
-	
-	t1=setTimeout(function() {
-	w1.window.close();
-	}, 3000);
 
-	t2=setTimeout(function() {
-	w2.window.close();
-	}, 5000);
+  var w3=window.open("http://www.weboniselab.com");
 
- 	t3=setTimeout(function() {
-	w3.window.close();
-	}, 7000);
-	t4=setTimeout(function() {
-	w4.window.close();
-	}, 9000);
+  var w4=window.open("https://www.amazon.com/");
 
-	t5=setTimeout(function() {
-	w5.window.close();
-	}, 11000);
+  var w5=window.open("https://www.snapdeal.com/");
+
+  t1=setTimeout(function() {
+    w1.window.close();
+  }, 3000);
+
+  t2=setTimeout(function() {
+    w2.window.close();
+  }, 5000);
+
+  t3=setTimeout(function() {
+    w3.window.close();
+  }, 7000);
+  t4=setTimeout(function() {
+    w4.window.close();
+  }, 9000);
+
+  t5=setTimeout(function() {
+    w5.window.close();
+  }, 11000);
 }
 
 function pause() {
-	clearTimeout(t1);
-	clearTimeout(t2);
-	clearTimeout(t3);
-	clearTimeout(t4);
-	clearTimeout(t5);
+  clearTimeout(t1);
+  clearTimeout(t2);
+  clearTimeout(t3);
+  clearTimeout(t4);
+  clearTimeout(t5);
 }
